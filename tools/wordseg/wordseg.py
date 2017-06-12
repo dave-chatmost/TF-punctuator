7b6cbfe85b2e8fde7e68ed054f8b2bdbd502e5fb
kaituoxu <kaituox@gmail.com>
2017-06-12 13:27:14 +0800

    change wordseg output path

--- tools/wordseg/wordseg.py
+++ tools/wordseg/wordseg.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+import os
 import sys
 
 
@@ -7,7 +8,7 @@ def max_match(sentence, dictionary):
     """Word segmentation in Chinese: the maximum matching algorithm.
     Args:
         sentence: Chinese character string.
-        dictionary: a list of Chinese words.
+        dictionary: dict of Chinese words.
     Return:
         Chinese words sequence list.
     """
@@ -18,7 +19,7 @@ def max_match(sentence, dictionary):
     for i in range(len(sentence), 0, -1): # len(sentence), ... , 1
         firstword = sentence[:i]
         remainder = sentence[i:]
-        if firstword in dictionary:
+        if dictionary.get(firstword):
             seq = [firstword]
             seq.extend(max_match(remainder, dictionary))
             return seq
@@ -38,13 +39,13 @@ def max_match(sentence, dictionary):
 
 def max_match_test():
     sentence = "ilovepythonhowaboutyou" # i love python how about you
-    dictionary = ["i", "we", "you", "your", "love", "python", "how", "about", "what", "lo", "yo"]
+    dictionary = {"i":1, "we":1, "you":1, "your":1, "love":1, "python":1, "how":1, "about":1, "what":1, "lo":1, "yo":1}
     word_seq = max_match(sentence, dictionary)
     print(word_seq)
     print((" ").join(word_seq))
 
 
-def word_seg(text_file_list_file, dict_file):
+def word_seg(text_file_list_file, dict_file, out_dir):
     """Do word segmentaion using maximum matching algorithm.
     Steps:
         1. load dictionary
@@ -55,27 +56,36 @@ def word_seg(text_file_list_file, dict_file):
     Args:
         text_file_list_file: a file contain a list of text file path.
         dict_file: dictionary file path.
+        out_dir: put the segmented file here.
     """
     def _load_dict(dict_file):
         with open(dict_file, 'r') as f:
-            dict = [word.strip('\n') for word in f]
+            dict = {word.strip('\n') : 1 for word in f}
         return dict
     
     dictionary = _load_dict(dict_file)
-    # print(dictionary)
     with open(text_file_list_file, 'r') as list:
         for file in list:
             file = file.strip()
+            out_file = os.path.join(out_dir, os.path.basename(file))
             print("Segmenting", file)
-            with open(file, 'r') as text, open(file+"_seg", 'w') as result:
+            print("Put the segmented file into", out_file)
+            with open(file, 'r') as text, open(out_file, 'w') as result:
                 for line in text:
-                    # print(line.strip('\n'))
                     sentence = line.strip()
-                    # print(max_match(sentence, dictionary))
-                    result.write(" ".join(max_match(sentence, dictionary)) + "\n")
+                    seg_sentence = max_match(sentence, dictionary)
+                    result.write(" ".join(seg_sentence) + "\n")
 
 
 if __name__ == "__main__":
+    if (len(sys.argv) < 3):
+        print("Usage: python wordseg.py <text-file-list-file> <dictionary-file>")
+        sys.exit(-1)
+
     text_file_list_file = sys.argv[1]
     dict_file = sys.argv[2]
-    word_seg(text_file_list_file, dict_file)
+    out_dir = os.path.join(os.getcwd(), "segment")
+    if not os.path.exists(out_dir):
+        os.mkdir(out_dir)
+
+    word_seg(text_file_list_file, dict_file, out_dir)
