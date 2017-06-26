edfab6e1c7a4438db18df89dd7f0ce4cf4515332
kaituoxu <kaituox@gmail.com>
2017-06-26 13:17:59 +0800

    test new format of tfrecord file(write and read)

---
+++ src/code_test/SequenceExample_read.py
@@ -0,0 +1,76 @@
+import tensorflow as tf 
+
+reader = tf.TFRecordReader()
+filename_queue = tf.train.string_input_producer(["Records/tfrecords-00001-of-00004"])
+_, serialized_example = reader.read(filename_queue)
+
+sequence_features = {
+    "inputs": tf.FixedLenSequenceFeature([], dtype=tf.int64),
+    "labels": tf.FixedLenSequenceFeature([], dtype=tf.int64)
+}
+
+_, sequence = tf.parse_single_sequence_example(
+    serialized=serialized_example,
+    sequence_features=sequence_features
+)
+inputs = sequence["inputs"]
+labels = sequence["labels"]
+
+batch_size=5
+batch = tf.train.batch(
+    tensors=[inputs, labels],
+    batch_size=batch_size,
+    dynamic_pad=True
+)
+input = batch[0]
+label = batch[1]
+
+init_state = tf.Variable(tf.zeros([], dtype=tf.int32))
+
+num_steps=3
+i = tf.train.range_input_producer(tf.size(input[0])//num_steps, shuffle=False).dequeue()
+inp_block = tf.strided_slice(input, [0, i * num_steps],
+                        [batch_size, (i + 1) * num_steps])
+lab_block = tf.strided_slice(label, [0, i * num_steps],
+                        [batch_size, (i + 1) * num_steps])
+
+
+def test_parse_single_seq_ex():
+    for i in range(8):
+        seq, inp, lab = sess.run([sequence, inputs, labels])
+        print(seq)
+        print(inp, lab)
+
+def test_batch():
+    for i in range(3):
+        bat = sess.run(batch)
+        print(bat[0])
+        print(bat[1])
+        print("*"*20)
+
+def test_block():
+    state = sess.run(init_state)
+    print(state)
+    for idx in range(8):
+        tempi, inp, lab = sess.run([i, inp_block, lab_block])
+        state = state + 1
+        if tempi == 0:
+            state = sess.run(init_state)
+        print(state)
+        print(tempi)
+        print(inp)
+        print(lab)
+        print("*"*20)
+
+
+with tf.Session() as sess:
+    sess.run(tf.global_variables_initializer())
+    coord = tf.train.Coordinator()
+    threads = tf.train.start_queue_runners(sess=sess, coord=coord)
+
+    #test_parse_single_seq_ex()
+    #test_batch()
+    test_block()
+
+    coord.request_stop()
+    coord.join(threads)
---
+++ src/code_test/SequenceExample_write.py
@@ -0,0 +1,57 @@
+import tensorflow as tf
+from random import randrange
+
+# fake data
+#sequences = [[1,2,3], [4,5,6,7], [8,9]]
+#label_sequences = [[2, 1, 1], [2, 3, 1, 1], [4, 3]]
+sequences = []
+label_sequences = []
+for i in range(20):
+    sequences.append([])
+    label_sequences.append([])
+    for e in range(randrange(5,20)):
+        sequences[-1].append(e+1)
+        label_sequences[-1].append(randrange(1,5))
+for i in range(20):
+    print(sequences[i], label_sequences[i])
+print("Above is fake data.")
+
+
+def make_example(sequence, labels):
+    ex = tf.train.SequenceExample()
+    fl_inputs = ex.feature_lists.feature_list["inputs"]
+    fl_labels = ex.feature_lists.feature_list["labels"]
+    for input, label in zip(sequence, labels):
+        fl_inputs.feature.add().int64_list.value.append(input)
+        fl_labels.feature.add().int64_list.value.append(label)
+    return ex
+
+
+def write_one_file():
+    filename = 'Records/output.tfrecords'
+    writer = tf.python_io.TFRecordWriter(filename)
+    for seq, label_seq in zip(sequences, label_sequences):
+        print(seq, label_seq)
+        ex = make_example(seq, label_seq)
+        writer.write(ex.SerializeToString())
+    writer.close()
+
+
+def write_many_file():
+    SENTENCES_PER_FILE = 5
+    NUM_FILES = len(sequences)//SENTENCES_PER_FILE
+    for i in range(NUM_FILES):
+        filename = 'Records/tfrecords-%.5d-of-%.5d' % (i+1, NUM_FILES)
+        writer = tf.python_io.TFRecordWriter(filename)
+        seqs = sequences[i*SENTENCES_PER_FILE: (i+1)*SENTENCES_PER_FILE]
+        labs = label_sequences[i*SENTENCES_PER_FILE: (i+1)*SENTENCES_PER_FILE]
+        for seq, label_seq in zip(seqs, labs):
+            print(seq, label_seq)
+            ex = make_example(seq, label_seq)
+            writer.write(ex.SerializeToString())
+        print("*"*20)
+        writer.close()
+
+
+if __name__ == "__main__":
+    write_many_file()
